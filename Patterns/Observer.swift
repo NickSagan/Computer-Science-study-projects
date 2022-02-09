b848a9bf251cf51735517f0ea034643578168bb6
Nikolai Saganenko <89981113+NickSagan@users.noreply.github.com>
2022-02-09 13:30:44 +0300

    Update Observer.swift

--- Patterns/Observer.swift
+++ Patterns/Observer.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+// SOLUTION 1 - with protocols
+
 protocol Observer {
     func notificationRecieved(_ message: String)
 }
@@ -51,3 +53,46 @@ teacher.addObserver(pupil1)
 teacher.homeTask = "Math: page 127, ex.: 3...7"
 pupil1.homeTask // observer: "Math: page 127, ex.: 3...7"
 pupil2.homeTask // not observer: "Nothing"
+
+// SOLUTION 2 - no protocols
+
+class Newspaper {
+    private var subscribers = NSMutableSet()
+    var newPaper: String = "" {
+        didSet {
+            notify(about: newPaper)
+        }
+    }
+    
+    func add(_ subscriber: Subscriber) {
+        subscribers.add(subscriber)
+    }
+    
+    func remove(_ subscriber: Subscriber) {
+        subscribers.remove(subscriber)
+    }
+    
+    private func notify(about newPaper: String) {
+        for sub in subscribers {
+            (sub as! Subscriber).recieve(newPaper)
+        }
+    }
+}
+
+class Subscriber: NSObject {
+    var myPaper: String = "Empty"
+    
+    func recieve(_ newPaper: String) {
+        myPaper = newPaper
+    }
+}
+
+let sub1 = Subscriber()
+let sub2 = Subscriber()
+let theTimes = Newspaper()
+
+theTimes.add(sub1)
+theTimes.newPaper = "The Navalny story"
+
+sub1.myPaper // observer: "The Navalny story"
+sub2.myPaper // not observer: "Empty"
