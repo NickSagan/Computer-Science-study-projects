090e9b7991a1926ab1fab35dbf1690f1863acb1f
Nikolai Saganenko <89981113+NickSagan@users.noreply.github.com>
2022-03-06 23:17:46 +0300

    Update Proxy.swift

--- Patterns/Proxy.swift
+++ Patterns/Proxy.swift
@@ -41,3 +41,51 @@ let user1 = User(name: "John")
 let proxy = ServerProxy()
 proxy.grantAccess(user: user1)
 proxy.denyAccess(user: user1)
+
+// Defence proxy
+
+class User2 {
+    var name: String
+    var password: String
+
+    init(name: String, password: String) {
+        self.name = name
+        self.password = password
+    }
+}
+
+protocol ServerProtocol2 {
+    func grantAccess(user: User2)
+}
+
+class ServerSide2: ServerProtocol2 {
+    
+    func grantAccess(user: User2) {
+        print("Access granted to user: \(user.name)")
+    }
+}
+
+class ServerProxy2: ServerProtocol2 {
+    
+    private var server: ServerSide2!
+    
+    func grantAccess(user: User2) {
+        guard server != nil else { print("Access denied"); return }
+        server.grantAccess(user: user)
+    }
+    
+    func authenticate(user: User2) {
+        guard user.password == "qwerty" else { return }
+        print("\(user.name) authenticated.")
+        server = ServerSide2()
+    }
+}
+
+let user2 = User2(name: "Harry", password: "qwe123")
+let proxy2 = ServerProxy2()
+proxy2.authenticate(user: user2)
+proxy2.grantAccess(user: user2)
+let user3 = User2(name: "Hermiona", password: "qwerty")
+proxy2.grantAccess(user: user3)
+proxy2.authenticate(user: user3)
+proxy2.grantAccess(user: user3)
