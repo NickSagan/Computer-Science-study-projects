85c91896e1df0acb11755eefc8f4390f22e32416
Nikolai Saganenko <89981113+NickSagan@users.noreply.github.com>
2022-04-16 11:39:40 +0300

    Update Unit2.py

--- UniversityOfThePeople/CS1101/Unit2.py
+++ UniversityOfThePeople/CS1101/Unit2.py
@@ -21,3 +21,16 @@ def clear_screen():
 nine_lines()
 print("Calling clear_screen()")
 clear_screen()
+
+# Journal
+
+name = "Nick Sagan"
+age = 33
+
+def greeting_with(greet):
+    result = greet + "! My name is " + name + " and I'm " + str(age) + " years old."
+    print(result)
+
+greeting_with("Hello")
+greeting_with("Salut")
+greeting_with("Hi")
